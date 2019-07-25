05ffc59d42f78a4475ebb0a2f983effcf500563b
sudeeptha967 <53205609+sudeeptha967@users.noreply.github.com>
2019-07-25 17:49:41 +0530

    Update runner.java

--- src/test/java/skeleton/runner.java
+++ src/test/java/skeleton/runner.java
@@ -7,7 +7,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/feature/total.feature",glue="pageobject",plugin="json:target\\Jsonreport.json")
+@CucumberOptions(features="src/test/resources/skeleton/total.feature",glue="cucumber-project-skeleton1",plugin="json:target\\Jsonreport.json")
 public class runner {
 	
 }
